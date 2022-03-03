77cd42957567aac1129dde8874b141ae6e73aee1
Banxter <po.prostu.adam1990@gmail.com>
2022-03-03 05:39:47 +0100

    + if, usunięcie [:-1] z ostatnich printów

--- najdluzsza_najkrotsza_domena.py
+++ najdluzsza_najkrotsza_domena.py
@@ -6,7 +6,10 @@ def najkrotsza_najdluzsza_domena( lista_flag):
     
     for flaga in lista_flag:
         dlugosc_flagi = len( flaga)
+        if '.' not in flaga and dlugosc_flagi == 0:
+            continue
         najkrotsza_najdluzsza.append( dlugosc_flagi)
+        print(dlugosc_flagi)
     return najkrotsza_najdluzsza
 
 
@@ -15,9 +18,8 @@ url = 'https://zajecia-programowania-xd.pl/flagi'
 lista_flag = stworz_liste_flag( url)
 
 najkrotsza_najdluzsza = najkrotsza_najdluzsza_domena( lista_flag)
-print("Najkrótsza nazwa domeny:",min(najkrotsza_najdluzsza[:-1]), "znaków.")
-
-print("Najdłuższa nazwa domeny:",max(najkrotsza_najdluzsza[:-1]), "znaków.")
+print("Najkrótsza nazwa domeny:",min(najkrotsza_najdluzsza), "znaków.")
+print("Najdłuższa nazwa domeny:",max(najkrotsza_najdluzsza), "znaków.")
 
 
 
