654d466dbc82fc30c9c18e1a501afdc5ca436c43
Banxter <po.prostu.adam1990@gmail.com>
2022-03-05 16:52:11 +0100

    Dodatkowy program liczący użytkowników którzy jeszcze nie postawili flagi

---
+++ domeny_z_hashtagiem.py
@@ -0,0 +1,13 @@
+from lista_flag import stworz_liste_flag
+
+def domeny_z_hashtagiem( lista_flag):
+    hash_domeny = []
+    
+    return 
+
+
+    
+url = 'https://zajecia-programowania-xd.pl/flagi'
+lista_flag = stworz_liste_flag( url)
+
+hash_domeny = domeny_z_hashtagiem( lista_flag)
