659ecd693546772f41f62cdeb175dd92a9b682a2
Banxter <po.prostu.adam1990@gmail.com>
2022-03-06 15:59:45 +0100

    alfa_list completed, creating alfa_date to start creating separate dates from text

---
+++ alfa_date.py
@@ -0,0 +1,18 @@
+from alfa_list import separate_data
+
+# Program which sort 1st column in a list called (Date)
+def only_date (date_list):
+    
+    
+    return
+
+
+
+url = 'http://www.po-prostu-adam.pl/alfa'
+date_list = separate_data(url)
+
+
+
+
+
+
---
+++ alfa_list.py
@@ -0,0 +1,16 @@
+import requests 
+
+def get_alfa_list(url):
+    raw_info = requests.get(url)
+    text = raw_info.text
+    return text
+
+def separate_data(url):
+    '''
+    Show every separate data from text in list
+    '''
+    text_from_site = get_alfa_list(url)
+    list_of_datas = text_from_site.split(',')
+    
+    return list_of_datas
+
