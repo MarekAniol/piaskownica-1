538fbb5d6a6721137e25b20a95968e149be84934
Banxter <po.prostu.adam1990@gmail.com>
2022-03-18 20:07:31 +0100

    Cosmetic

--- alfa_date.py
+++ alfa_date.py
@@ -2,16 +2,21 @@ from alfa_list import separate_data
 
 # Program which sort 1st column in a list called (Date)
 def only_date(date_list):
-    
-    clean_date = []
-    
-    for el in date_list:
-        
-    return el
+    lst = [l.split("\n") if "\n" in l else l for l in date_list]
+    separate_datas = []
+    for el in lst:
+        if len(el) == 2:
+            for e in el:
+                separate_datas.append(e)
+        else:
+            separate_datas.append(el)
+    result = [separate_datas[i:i + 13] for i in range(0, len(separate_datas), 13)]
+    return result
 
 url = 'http://www.po-prostu-adam.pl/alfa'
 date_list = separate_data(url)
-el = only_date (date_list)
+result = only_date(date_list)
+print(result)
 # print(clean_date)
 # clean_date = only_date(date_list)
 # print(clean_date)
--- alfa_list.py
+++ alfa_list.py
@@ -1,33 +1,31 @@
 import requests
 
-url = 'http://www.po-prostu-adam.pl/alfa'
-
 def get_alfa_list(url):
     raw_info = requests.get(url)
     text = raw_info.text
     return text
 
+
 def separate_data(url):
-    '''
-    Show every separate data from text in list
-    '''
     text_from_site = get_alfa_list(url)
-    list_of_datas = text_from_site.split(',')
-    
-   
-
+    list_of_datas = text_from_site.split("</p>")[0].split("<p>")[-1]
+    list_of_datas = list_of_datas.strip()
+    list_of_datas = list_of_datas.split(",")
     return list_of_datas
-        
 
-       
-print(separate_data(url))
-       
-       
-       
-        
-    # for c in new_list:
-    #     clean.append(c.replace('\n',' '))
-    # for el in clean:
-    #     if ' ' in el:
-    #         clean.append(c.split(' '))
-    # return c
+
+def only_date(date_list):
+    lst = [l.split("\n") if "\n" in l else l for l in date_list]
+    separate_datas = []
+    for el in lst:
+        if len(el) == 2:
+            for e in el:
+                separate_datas.append(e)
+        else:
+            separate_datas.append(el)
+    result = [separate_datas[i:i + 13] for i in range(0, len(separate_datas), 13)]
+    return result
+
+url = 'http://www.po-prostu-adam.pl/alfa'
+date_list = separate_data(url)
+final_data = only_date(date_list)
